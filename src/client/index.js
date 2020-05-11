1d3ee5e33a34f6295350f2a94f2dfacee5a22e78
SupTrip <55024921+SupTrip@users.noreply.github.com>
2020-05-11 00:41:13 -0400

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -11,16 +11,6 @@ window.addEventListener("DOMContentLoaded", (_) => {
   document.getElementById("button").addEventListener("click", performAction);
 });
 
-<<<<<<< HEAD
-=======
-import {  performAction} from "./js/app";
-
- //document.getElementById('generate').addEventListener('click',performAction)
-window.addEventListener('DOMContentLoaded', _ =>{
-	document.getElementById('generate').addEventListener('click',performAction);
-});
-alert("I EXIST")
->>>>>>> 40d2698c402768d1ceb02b9a2c0d258a6c9ac0cc
 console.log("CHANGE!!");
 
 export { performAction };
